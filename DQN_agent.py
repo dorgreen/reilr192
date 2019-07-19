b9c3f5b054070c38ff98db98097be835f65cd4a3
Dor Green <dor@cdi-negev.com>
2019-07-19 12:55:29 +0300

    training! start of menu system

--- DQN_agent.py
+++ DQN_agent.py
@@ -10,13 +10,17 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 
 
+WIDTH = 210
+HEIGHT = 160
+
 # does not inherit from object to make it lighter. could inherit later if needed
 class DQNAgent:
     def __init__(self, action_space):
         self.action_space = action_space
-        self.epsilon = 1.0 # epsilon changes with "temperture"
+        self.epsilon = 1.0 # epsilon changes with "temperture", resets on each episode
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.98
+        self.gamma = 0.9 # discount
         self.batch_size = 150
         self.learning_rate = 0.02
         self.memory = list()
@@ -38,8 +42,8 @@ class DQNAgent:
     def get_action(self, state, reward, done):
         """epsilon-greedy"""
         if random.random() > self.epsilon:
-            action_vector = self.predict(state)
-            return tf.arg_max(action_vector)
+            action_vector = self.model.predict(state)
+            return np.argmax(action_vector)
 
         else:
             return random.randrange(start=0, stop=self.action_space.n)
@@ -47,47 +51,54 @@ class DQNAgent:
     # add this transition to memory
     #      TODO: Add feature that saves image of state once in a while for debug
     def remember(self, state, action, reward, next_state, done):
-        if(len(self.memory) >= 2000):
+        if len(self.memory) >= 2000:
             self.memory.pop()
         self.memory.append((state, action,reward,next_state,done))
         return
 
     # train model with the new data in memory
     #      TODO: IMPLEMENT
+    #      TODO: add TensorBoard callback on model.predict for metrics report
     def replay(self, batch_size):
         # update epsilon
         if self.epsilon_min > self.epsilon:
             self.epsilon = self.epsilon * self.epsilon_decay
 
         batch = random.sample(self.memory, batch_size)
+
         # for each transition in this batch, set target and fit to model accordingly
+
+        #states = list(map(lambda x: x[0], batch))
+        #targets = self.model.predict(states, batch_size, 1)
+
         for state, action, reward, next_state, done in batch:
-            target = self.model.predict(np.array(state).flatten())
+            target = reward
+
             if not done:
-                a = self.model.predict(np.array(next_state).flatten())[0]
-                t = self.target_model.predict(next_state)[0]
-                target[0][action] = reward + self.gamma * t[np.argmax(a)]
-            else:
-                target[0][action] = reward
-            self.model.fit(state, target, epochs=1, verbose=0)
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)))
+
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            loss = self.model.fit(state, target_f, epochs=1, verbose=0)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
         return
 
-    #      TODO: IMPLEMENT
+    #      TODO: TEST
     def load_weights_from_file(self, filename):
         return self.model.set_weights(filename)
 
-    #      TODO: IMPLEMENT
+    #      TODO: TEST
     def save_weights_to_file(self, filename):
         return self.model.save_weights(filename, True)
 
-WIDTH = 210
-HEIGHT = 160
 
 """turns a single frame from original format to the format in Q function"""
 def resize_frame(ob):
     # TODO: if changing network input, change here
-    return rgb2gray(ob)
+    return np.array(rgb2gray(ob)).flatten()
 
 temp_env = gym.make('DemonAttack-v0')
 empty_frame = resize_frame(temp_env.reset())
@@ -108,14 +119,14 @@ def obs_to_state(ob):
     frames_buffer.pop(0) # oldest frame discarded
     frames_buffer.append(this_frame)
 
-    # return ob;
-    return tuple(frames_buffer)
+    # flatten from 4 * 33600 to 134400 as needed
+    return np.array(tuple(frames_buffer)).flatten().reshape([1, WIDTH*HEIGHT*4])
 
 if __name__ == '__main__':
 
     # TODO: set args to know if we should train or load from disk and just play according to model
     # TODO: set training via GPU?
-    # TODO: add TensorBoard for logging and reporting. can I do it without using TF scoping?
+    # TODO: add TensorBoard for logging and reporting. [could be used without TF scoping?]
 
 
     # You can set the level to logger.DEBUG or logger.WARN if you
@@ -124,46 +135,101 @@ if __name__ == '__main__':
 
     env = gym.make('DemonAttack-v0')
 
-    # possible tempfile.mkdtemp().
+    mode = "train"
 
+    ## Setup monitor if needed
+    ## possible to use tempfile.mkdtemp().
     # outdir = '/tmp/random-agent-results'
     # env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
     agent = DQNAgent(env.action_space)
 
-    episode_count = 100
-    episode_length = 5000
-    reward = 0
-    done = False
+    if mode == "train":
+        episode_count = 100
+        episode_length = 5000
+        reward = 0
+        done = False
 
-    for i in range(episode_count):
-        ## Original obervation is an nd array of (210, 160, 3) , such that H * W * rgb
+        for i in range(episode_count):
+            ## Original obervation is an nd array of (210, 160, 3) , such that H * W * rgb
+            reset_frame_buffer()
+            ob = env.reset()
+            state = obs_to_state(ob)
+            prev_state = state
+            action = 0
+            agent.epsilon = 1
+
+            # DEBUG ONLY
+            print("EPISODE: {}".format(i))
+
+
+            ## for each episode we play according to agent for (at most) episode_length frames
+            ## after X frames, update agent's model using the new data we gathered.
+            for t in range(episode_length):
+                action = agent.get_action(state, reward, done)
+                ob, reward, done, _ = env.step(action)
+                prev_state = state
+                state = obs_to_state(ob)
+                agent.remember(prev_state, action, reward, state, done)
+
+                trained = False
+
+                env.render()
+                if done:
+                    reset_frame_buffer()
+                    break
+
+                # env.monitor can record video of some episodes. see capped_cubic_video_schedule
+
+                # DEBUG ONLY
+                if(t % 250 == 0):
+                    print("episode: {} step {}".format(i,t))
+
+                if(t % 1000 == 0 and t>0):
+                    agent.replay(agent.batch_size)
+                    trained = True
+
+            # Train after each episode (for when we didn't quite make it to 1000 frames...)
+            if (not trained) and agent.memory.__len__() > agent.batch_size:
+                agent.replay(agent.batch_size)
+
+            # Save every 25 runs
+            if i % 25 == 0:
+                agent.save_weights_to_file("./save{}.h5".format(i))
+                agent.save_weights_to_file("./lastest.h5")
+
+            # Close the env (and write monitor result info to disk if it was setup)
+            env.close()
+
+    #todo debug only
+    mode = "play"
+
+    if mode == "play":
+        #agent.load_weights_from_file("./latest.h5")
+
+        # Reset everything...
         reset_frame_buffer()
         ob = env.reset()
         state = obs_to_state(ob)
-        prev_state = state
         action = 0
+        reward = 0
+        done = 0
+        agent.epsilon = agent.epsilon_min
+        total_reward = 0
 
-        ## for each episode we play according to agent for (at most) episode_length frames
-        ## after X frames, update agent's model using the new data we gathered.
-        for t in range(episode_length):
+        # play one game
+        print("play one game")
+        for t in range(100000):
             action = agent.get_action(state, reward, done)
             ob, reward, done, _ = env.step(action)
-            prev_state = state
             state = obs_to_state(ob)
-            agent.remember(prev_state, action, reward, state, done)
+            total_reward += reward
 
             env.render()
             if done:
+                reset_frame_buffer()
+                print("total reward: {}".format(total_reward))
                 break
 
-            # env.monitor can record video of some episodes. see capped_cubic_video_schedule
 
-            if(t % 1000 == 0 and t>0):
-                agent.replay(agent.batch_size)
 
-        # Train after each episode (for when we didn't quite make it to 1000 frames...)
-        if agent.memory.__len__() > agent.batch_size: agent.replay(agent.batch_size)
-
-    # Close the env and write monitor result info to disk
-    env.close()
